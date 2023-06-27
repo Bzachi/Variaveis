270225e24f96403a5652cbc6724c582c60a9a6ff
Beatriz Zachi <134453466+Bzachi@users.noreply.github.com>
2023-06-26 22:49:27 -0300

    Add files via upload

---
+++ variáveis/01.js
@@ -0,0 +1,17 @@
+var mensagem = "Página Carregada com Sucesso!";
+        alert(mensagem);
+        
+        var x = 6;
+        var y = 3;
+
+        var z = x + y;
+        document.getElementById("soma").innerHTML = z;
+
+        var z = x / y;
+        document.getElementById("divisao").innerHTML = z;
+
+        var z = x * y;
+        document.getElementById("multi").innerHTML = z;
+
+        var z = x < y;
+        document.getElementById("comparacao").innerHTML = z;
